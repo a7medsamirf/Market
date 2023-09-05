ce204c3002a879e26511f314c1deb9b788cfe9be
ahmed samir <ahmed.samir.farid@gmail.com>
2023-09-05 18:06:17 +0300

    V 0.1

---
+++ text.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>{{ $t('products.heading') }}</h1>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          {{ $t(`products.${product.id}.name`) }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    async asyncData({ params, app }) {
+      // Fetch the products based on the current locale
+      const locale = app.i18n.locale;
+      const products = await fetch(`/api/products_${locale}.json`).then((res) => res.json());
+  
+      return { products };
+    },
+  };
+  </script>
+  
